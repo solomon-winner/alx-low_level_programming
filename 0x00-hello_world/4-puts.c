4828028f36c02df492f5878c5e1fc90aaa0acd78
solomon-winner <solyalew3@gmail.com>
2023-02-16 18:00:37 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(void)
-{/**
-   *this is the main function that is the Entry point
-   * 
-   *it will always return 0 that is success
-   *
-   */
+{
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
