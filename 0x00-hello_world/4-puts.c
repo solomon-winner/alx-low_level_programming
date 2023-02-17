57c5cd843978e8198a0b64074a597ded437661f8
solomon-winner <solyalew3@gmail.com>
2023-02-16 18:05:08 -0800

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-/**
+
+	/**
  * main - Entry Point
  *
  * Return: Always 0 (Success)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
