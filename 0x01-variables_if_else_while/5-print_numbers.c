62683de92ac2d8a4c193c7892918548b7e66ee2b
solomon-winner <solyalew3@gmail.com>
2023-02-19 15:31:45 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char single = 0;
+
+	while (single < 10)
+	{
+		printf("%d",single);
+		single++;
+	}
+	return (0);
+}
+
