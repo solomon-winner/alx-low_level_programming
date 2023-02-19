815d3059695a1bb079d70cd45bf9871bf3021ba6
solomon-winner <solyalew3@gmail.com>
2023-02-19 12:26:22 -0800

    alx project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n<0){
-		printf("%d", n , " is a negative\n")
+		printf("%d is negative\n", n)
 	}
 	else if(n=0){
-		printf("%d", n , " is zero\n")
+		printf("%d is zero\n", n)
 	}
 	else(n>0){
-		printf("%d", n , " is negative\n")
+	printf("%d is positive\n", n)	
 	}
 	return (0);
 }
