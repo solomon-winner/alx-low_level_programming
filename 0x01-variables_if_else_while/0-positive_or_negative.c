03ea090734d2b783e9eba8d1e4871ea0b3ab4ab2
solomon-winner <solyalew3@gmail.com>
2023-02-19 12:34:08 -0800

    alx project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,11 @@ int main(void)
 	if (n<0){
 		printf("%d is negative\n", n);
 	}
-	else if(n=0){
+ 	else if(n>0){
+        printf("%d is positive\n", n);
+        }
+	else{
 		printf("%d is zero\n", n);
-	}
-	else(n>0){
-	printf("%d is positive\n", n);
 	}
 	return (0);
 }
