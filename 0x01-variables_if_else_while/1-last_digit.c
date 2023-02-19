1543e82e0f35ddd1d05e046f2eed9cbdd9427492
solomon-winner <solyalew3@gmail.com>
2023-02-19 13:42:38 -0800

    this is alx- project

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,13 +23,12 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	else if (last > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, last);
-        }
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
 	else if ((last < 6) && (last != 0))
-        {
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-        
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
