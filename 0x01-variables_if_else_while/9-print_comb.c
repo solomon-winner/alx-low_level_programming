142cdcad0457b7fa587ae34e2e171ca328ac076e
solomon-winner <solyalew3@gmail.com>
2023-02-19 16:33:34 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int singleDigit = 0;
+
+	singleDigit = '0';
+	while(singleDigit <= '9')
+	{
+		putchar(singleDigit);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
