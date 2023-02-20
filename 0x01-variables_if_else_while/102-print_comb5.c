249f533ef776b756d847dc5b66e0ba3f5aa52b31
solomon-winner <solyalew3@gmail.com>
2023-02-19 17:47:29 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (count != 4949)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+			c++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
