72f138fdce48bd0b0f6cb77304707518b15f44e6
solomon-winner <solyalew3@gmail.com>
2023-02-22 18:11:32 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,20 +5,30 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-	while (n < 98)
-	{
-		_putchar(n);
-		n++;
+        if (n < 98)
+        {
+                while (n <= 98)
+                {
+                        if (n = 98)
+                        {
+                                _putchar(n + '0');
+                        }
+			_putchar(n + '0');
+			_putchar(',' + ' ');
+			n++;
+		}
 	}
-	}
-	else if(n > 98)
+	else if (n > 98)
 	{
-		while (n >98)
+		while (n >= 98)
 		{
-		_putchar(n);
-		n--;
+			if (n = 98)
+			{
+				_putchar(n + '0');
+			}
+			_putchar(n + '0');
+			_putchar(',' + ' ');
+			n--;
 		}
 	}
 	else
