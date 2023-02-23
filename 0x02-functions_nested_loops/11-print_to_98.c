8521527adb9497c28f862e6589144237bf8eb122
solomon-winner <solyalew3@gmail.com>
2023-02-22 17:52:07 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the parameter passed to this function
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+	while (n < 98)
+	{
+		_putchar(n);
+		n++;
+	}
+	}
+	else if(n > 98)
+	{
+		while (n >98)
+		{
+		_putchar(n);
+		n--;
+		}
+	}
+	else
+	{
+		_putchar(n);
+	}
+}
