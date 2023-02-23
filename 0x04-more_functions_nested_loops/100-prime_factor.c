7e96d51b461664cb2e4bd1c436b1aeb33f4a907f
solomon-winner <solyalew3@gmail.com>
2023-02-23 14:59:07 -0800

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the largest prime factor 
+ * main - prints the prime factor 
  * Return: 0
  */
 int main(void)
 {
-	unsigned long int i = 3, n = 612852475143;
+	unsigned long int i, n = 612852475143;
 
-	for (; i < 12057; i += 2)
+	for (i = 3; i < 12057; i += 2)
 	{
 		while (n % i == 0 && n != i)
 			n /= i;
