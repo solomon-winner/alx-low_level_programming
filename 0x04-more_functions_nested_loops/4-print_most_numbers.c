269f07c4e12095197124d3ac9edf39b01832ec65
solomon-winner <solyalew3@gmail.com>
2023-02-23 12:45:29 -0800

    what does it mean 'I believe in numbers'

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char start = '0';
+
+	while (start <= '9')
+	{
+		if (start !== 2 && start != 4)
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+}
