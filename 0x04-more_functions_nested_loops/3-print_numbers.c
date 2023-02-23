cf84f67a203f9862f6af7ae82af1e58974926fc0
solomon-winner <solyalew3@gmail.com>
2023-02-23 12:29:14 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number
+ */
+void print_numbers(void)
+{
+	char start = '0';
+	while (start <= '9')
+	{
+		_putchar(start);
+	}
+	_putchar('\n');
