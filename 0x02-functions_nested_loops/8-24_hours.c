23f85a7f70b40a46ad2ef0c88b35bc1276b111c9
solomon-winner <solyalew3@gmail.com>
2023-02-22 16:46:51 -0800

    . I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,12 @@ void jack_bauer(void)
 {
 	int minute;
 	int second;
-	char z;
-	char zs;
 
 	for (minute = 0; minute <= 23; minute++)
 	{
 		if (minute < 10 && minute >= 0)
 		{
 			minute = minute + '0';
-			z = 0 + '0';
 			minute = minute + minute;
 		}
 		for (second = 0; second < 60; second++)
@@ -24,7 +21,6 @@ void jack_bauer(void)
 			if (second < 10 && second >= 0)
 			{
 				second = second + '0';
-				zs = 0 + '0';
 				second = second + second;
 			}
 			_putchar(minute +  ':' + second);
