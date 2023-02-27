3d4440857c962fc8d8747b6919645441d2add13b
solomon-winner <solyalew3@gmail.com>
2023-02-27 10:15:34 -0800

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * main - generates random pass word
+ * Return: Always 0
+ */
+int main(void)
+{
+	char password[84];
+	int index = 0;
+	int sum = 0;
+	int a;
+	int b;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		a = (sum - 2772) / 2;
+		b = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			a++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + a))
+			{
+				password[index] -= a;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + b))
+			{
+				password[index] -= b;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
+}
