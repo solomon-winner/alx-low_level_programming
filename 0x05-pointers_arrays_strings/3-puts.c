8179f336f3316ac7e19f1675254fa78822859aac
solomon-winner <solyalew3@gmail.com>
2023-02-27 02:39:50 -0800

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,13 @@
 
 /**
  *_puts - prints a string
- *@str: pointer of string 
+ *@str: pointer of string
  *Return: void
  */
 void _puts(char *str)
 {
 	int counter = 0;
+
 	while (str[counter] != '\0')
 	{
 		_putchar(str[counter]);
