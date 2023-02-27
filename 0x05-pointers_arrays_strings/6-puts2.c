46bd9decb416a39a0ffe411003586f48d14722f7
solomon-winner <solyalew3@gmail.com>
2023-02-27 04:51:51 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,16 +9,17 @@ void puts2(char *str)
 {
 	int counter = 0;
 	int c = 0;
+	int m;
 
 	while (str[counter] != '\0')
 	{
 		counter++;
 	}
 	_putchar(str[0]);
-	while (c + 2 <= counter - 1)
+	while (c <= counter - 1)
 	{
-		_putchar(str[c + 2]);
-		c++;
+		_putchar(str[c]);
+		c + 2;
 	}
 	_putchar('\n');
 }
