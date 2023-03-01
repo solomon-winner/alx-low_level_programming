202f130fb5a33fea748f385af902192be9a3a7fe
solomon-winner <solyalew3@gmail.com>
2023-03-01 12:30:52 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int loop = 0;
+	int count = 0;
 
-	while ((s1[count] != '\0' && s2[count] != '\0') && (s1[count] == s2[count]))
+	while ((s1[count] != '\0' && s2[count] != '\0') && s1[count] == s2[count])
 	{
 		count++;
 	}
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else
-	{
-		return (s1[count] - s2[count]);
-	}
+	return (s1[count] - s2[count]);
 }
