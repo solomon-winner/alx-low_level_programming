cbfc9cd0b5bfeb3557269551803cc7f3d60aecc1
solomon-winner <solyalew3@gmail.com>
2023-03-01 15:41:48 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *a)
 
 	while (a[counter] != '\0')
 	{
-		if ((a[counter] >= 32 && a[counter] <= 47) && ((a[counter] >= 58 && a[counter] <= 64) && (a[counter] >= 134 && a[counter] <=149)))
+		if ((a[counter] >= 32 && a[counter] <= 47) || ((a[counter] >= 58 && a[counter] <= 64) || (a[counter] >= 134 && a[counter] <= 149)))
 		{
 			start = counter + 1;
 			if (a[start] <= 122 && a[start] >= 97)
