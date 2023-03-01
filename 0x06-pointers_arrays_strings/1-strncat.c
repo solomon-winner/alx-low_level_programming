2ca232ec519b6c210978e3bc01fe9dca9d6e2ae9
solomon-winner <solyalew3@gmail.com>
2023-03-01 10:09:41 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: a pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0;
+	int loop;
+
+	while (dest[index] != '\0')
+	{
+		index++;
+	}
+
+	for (loop = 0; loop < n; loop++)
+	{
+		dest[index] = src[loop];
+		index++;
+	}
+	return (dest);
+}
