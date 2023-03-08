71380d2b750cc1f28967ec3265a8c26670641616
solomon-winner <solyalew3@gmail.com>
2023-03-08 11:28:05 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,10 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*s);
+		_puts_recursion(*s + 1);
 	}
 	_putchar('\n');
 }
