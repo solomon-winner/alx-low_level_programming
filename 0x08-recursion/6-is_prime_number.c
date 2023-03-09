093f1e61063c2da86f49d3928790b6379296cf7b
solomon-winner <solyalew3@gmail.com>
2023-03-09 08:13:17 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,10 @@ int is_prime_number(int n)
 {
 	if (n != 2 && n != 3 && n != 5 && n != 7)
 	{
-		if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+		if (n == 0 || n == 1)
+			return (0);
+		else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
 			return (0);
 	}
-	else if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (0);
 	return (1);
 }
