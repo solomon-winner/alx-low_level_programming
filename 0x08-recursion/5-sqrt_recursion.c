49166480436efd2ea8f0979f8006bb611216f94a
solomon-winner <solyalew3@gmail.com>
2023-03-09 01:47:18 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,27 +9,29 @@ int _sqrt_recursion(int n)
 {
 	if (n % 2 == 0)
 	{
-		if ( n/2 * n/2 == n)
-			return (n/2);
-		_sqrt_recursion(n/2);
+		if ( n / 2 * n / 2 == n)
+			return (n / 2);
+		_sqrt_recursion(n / 2);
 	}
 	else if (n % 3 == 0)
 	{
-		if (n/3 * n/3 == n)
-			return (n/3);
-		_sqrt_recursion(n/3);
+		if (n / 3 * n / 3 == n)
+			return (n / 3);
+		_sqrt_recursion(n / 3);
 	}
 	else if (n % 5 == 0)
 	{
-		if (n/5 * n/5 == n)
-			return (n/5);
-		_sqrt_recursion(n/5);
+		if (n / 5 * n / 5 == n)
+			return (n / 5);
+		_sqrt_recursion(n / 5);
 	}
 	else if (n % 7 == 0)
 	{
-		if (n/7 * n/7 == n)
-			return (n/7);
-		_sqrt_recursion(n/7);
+		if (n / 7 * n / 7 == n)
+			return (n / 7);
+		_sqrt_recursion(n / 7);
 	}
+	else if (n == 1)
+		return (1);
 	return (-1);
 }
