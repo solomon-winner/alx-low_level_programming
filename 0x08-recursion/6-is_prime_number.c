5ebba4086906bbc443bbd58f2066539791efa4d9
solomon-winner <solyalew3@gmail.com>
2023-03-09 08:03:59 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if the number is prime number
+ * @n: parameter
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n != 2 && n != 3 && n != 5 && n != 7)
+	{
+		if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+			return (0);
+	}
+	else if (n == 0)
+		return (0);
+	return (1);
+}
