72995c84e5ed5151421f7467fb08c98125db986e
solomon <solyalew3@gmail.com>
2023-06-09 14:50:12 -0700

    It ain't what they call you, it's what you answer to

---
+++ 1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments passed into the program
+ * @argv: array of strings of the arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
